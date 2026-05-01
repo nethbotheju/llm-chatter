ad65430bed076caed7f03d565164888f27d8df9e
nethbotheju <nethbotheju@gmail.com>
2026-05-01 21:35:29 +0530

    feat: sidebar chat item enhancements

--- src/components/sidebar/conversation-list.tsx
+++ src/components/sidebar/conversation-list.tsx
@@ -26,7 +26,7 @@ export function ConversationList({
   }
 
   return (
-    <div className="flex flex-col gap-1 p-2">
+    <div className="flex flex-col gap-0.5 p-2">
       {conversations.map((conversation) => (
         <ConversationItem
           key={conversation.id}
